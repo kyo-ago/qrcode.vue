273be1113fc788a0d523f28c54ac2f0a297d52dc
kyo-ago <kyo.ago@gmail.com>
2019-07-23 18:04:40 +0900

    change qr.js to qrcode

--- src/index.js
+++ src/index.js
@@ -1,41 +1,4 @@
-import QRCode from 'qr.js/lib/QRCode'
-import ErrorCorrectLevel from 'qr.js/lib/ErrorCorrectLevel'
-
-/**
- * Encode UTF16 to UTF8.
- * See: http://jonisalonen.com/2012/from-utf-16-to-utf-8-in-javascript/
- * @param str {string}
- * @returns {string}
- */
-function toUTF8String(str) {
-  let utf8Str = ''
-  for (let i = 0; i < str.length; i++) {
-    let charCode = str.charCodeAt(i)
-    if (charCode < 0x0080) {
-      utf8Str += String.fromCharCode(charCode)
-    } else if (charCode < 0x0800) {
-      utf8Str += String.fromCharCode(0xc0 | (charCode >> 6))
-      utf8Str += String.fromCharCode(0x80 | (charCode & 0x3f))
-    } else if (charCode < 0xd800 || charCode >= 0xe000) {
-      utf8Str += String.fromCharCode(0xe0 | (charCode >> 12))
-      utf8Str += String.fromCharCode(0x80 | ((charCode >> 6) & 0x3f))
-      utf8Str += String.fromCharCode(0x80 | (charCode & 0x3f))
-    } else {
-      // surrogate pair
-      i++
-      // UTF-16 encodes 0x10000-0x10FFFF by
-      // subtracting 0x10000 and splitting the
-      // 20 bits of 0x0-0xFFFFF into two halves
-      charCode =
-        0x10000 + (((charCode & 0x3ff) << 10) | (str.charCodeAt(i) & 0x3ff))
-      utf8Str += String.fromCharCode(0xf0 | (charCode >> 18))
-      utf8Str += String.fromCharCode(0x80 | ((charCode >> 12) & 0x3f))
-      utf8Str += String.fromCharCode(0x80 | ((charCode >> 6) & 0x3f))
-      utf8Str += String.fromCharCode(0x80 | (charCode & 0x3f))
-    }
-  }
-  return utf8Str
-}
+import * as qrcode from 'qrcode'
 
 const QrcodeVue = {
   render(createElement) {
@@ -44,7 +7,7 @@ const QrcodeVue = {
     return createElement(
       'div',
       {
-        class: this.class || className,
+        class: className,
         attrs: { value, level, background, foreground },
       },
       [
@@ -62,9 +25,8 @@ const QrcodeVue = {
   },
   props: {
     value: {
-      type: String,
+      type: undefined,
       required: true,
-      default: '',
     },
     className: {
       type: String,
@@ -94,29 +56,26 @@ const QrcodeVue = {
       const { value, size, level, background, foreground } = this
       const _size = size >>> 0 // size to number
 
-      // We'll use type===-1 to force QRCode to automatically pick the best type
-      const qrCode = new QRCode(-1, ErrorCorrectLevel[level])
-      qrCode.addData(toUTF8String(value))
-      qrCode.make()
-
       const canvas = this.$refs['qrcode-vue']
 
-      const ctx = canvas.getContext('2d')
-      const cells = qrCode.modules
-      const tileW = _size / cells.length
-      const tileH = _size / cells.length
-      const scale = window.devicePixelRatio || 1
-      canvas.height = canvas.width = _size * scale
-      ctx.scale(scale, scale)
-
-      cells.forEach(function(row, rdx) {
-        row.forEach(function(cell, cdx) {
-          ctx.fillStyle = cell ? foreground : background
-          const w = Math.ceil((cdx + 1) * tileW) - Math.floor(cdx * tileW)
-          const h = Math.ceil((rdx + 1) * tileH) - Math.floor(rdx * tileH)
-          ctx.fillRect(Math.round(cdx * tileW), Math.round(rdx * tileH), w, h)
-        })
-      })
+      qrcode.toCanvas(
+        canvas,
+        value,
+        {
+          errorCorrectionLevel: level,
+          width: _size,
+          color: {
+            dark: background,
+            light: foreground,
+          },
+        },
+        function(error) {
+          if (error) {
+            console.error(error)
+            throw error
+          }
+        }
+      )
     },
   },
   updated() {
